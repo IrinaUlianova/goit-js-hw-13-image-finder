4d0a3cebe60bf80881a28db4ed153cc804081fe3
Irina Ulianova <ulianovira.89@gmail.com>
2021-08-29 17:01:10 +0300

    add load-btn

---
+++ src/apiService.js
@@ -0,0 +1,35 @@
+// const API_KEY = '23040778-e9275b0b863c22e4eb8641cfc';
+const BASE_URL = 'https://pixabay.com';
+export default class NewsApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  fetchArticles() {
+    //   const options = {
+    //     headers: {
+    //       Authorization: '23040778-e9275b0b863c22e4eb8641cfc',
+    //     },
+    //   };
+    const url = `${BASE_URL}/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=23040778-e9275b0b863c22e4eb8641cfc`;
+    return fetch(url)
+      .then(response => response.json())
+      .then(({ hits }) => {
+        this.incrementPage();
+        return hits;
+      });
+  }
+  incrementPage() {
+    this.page += 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
+import './css/common.css';
 import './sass/main.scss';
+import NewsApiService from './apiService';
+import imageCardTpl from './templates/image-card.hbs';
+import LoadMoreBtn from './load-more-btn';
+const refs = {
+  searchForm: document.querySelector('.js-search-form'),
+  articlesContainer: document.querySelector('.js-gallery-container'),
+  // loadMoreBtn: document.querySelector('[data-action="load-more"]'),
+};
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
+const newsApiService = new NewsApiService();
+
+// loadMoreBtn.enable();
+refs.searchForm.addEventListener('submit', onSearch);
+loadMoreBtn.refs.button.addEventListener('click', onLoadMore);
+
+let searchQuery = '';
+function onSearch(e) {
+  e.preventDefault();
+
+  newsApiService.query = e.currentTarget.elements.query.value;
+  if (newsApiService.query === '') {
+    return alert('Неправильный ввод');
+  }
+  loadMoreBtn.show();
+  loadMoreBtn.disable();
+  newsApiService.resetPage();
+  newsApiService.fetchArticles().then(hits => {
+    clearArticlesContainer();
+    appendArticlesMarkUp(hits);
+  });
+}
+function onLoadMore() {
+  newsApiService.fetchArticles().then(appendArticlesMarkUp);
+}
+function appendArticlesMarkUp(hits) {
+  refs.articlesContainer.insertAdjacentHTML('beforeend', imageCardTpl(hits));
+}
+function clearArticlesContainer() {
+  refs.articlesContainer.innerHTML = '';
+}
